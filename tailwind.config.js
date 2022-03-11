72796822a2d05c24b61099156751a66d66de7691
fajar7xx <fajar7xx@gmail.com>
2022-03-11 12:09:28 +0700

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,25 @@
+module.exports = {
+  content: ["./public/**/*.{html, js}"],
+  theme: {
+    screen: {
+      sm: "576px",
+      md: "768px",
+      lg: "992px",
+      xl: "1200px",
+    },
+    container: {
+      center: true,
+      padding: "1rem",
+    },
+    extend: {
+      fontFamily: {
+        poppins: ["Poppins", "sans-serif"],
+        roboto: ["Roboto", "sans-serif"],
+      },
+      colors: {
+        primary: "#fd3d57",
+      },
+    },
+  },
+  plugins: [],
+};
